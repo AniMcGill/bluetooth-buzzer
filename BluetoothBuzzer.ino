687d87378b184e16a11bc8069e3708ac5c1b0387
Natsumi Hoshino <me@kagehoshi.com>
2013-10-04 08:58:58 -0400

    Initial commit

---
+++ BluetoothBuzzer.ino
@@ -0,0 +1,36 @@
+/*
+  BluetoothBuzzer
+  When a button is pressed, sends a keypress over serial (bluetooth)
+ */
+ 
+// Define the default number of this buzzer
+int buzzerNumber = 4; //edit this
+
+// Define the pin corresponding to the buzzer button
+int buzzerBtn = 2;
+
+void setup() {
+  pinMode(buzzerBtn, INPUT);
+  digitalWrite(buzzerBtn, HIGH); //pull-up resistor
+  Serial.begin(9600);
+}
+
+void loop() {
+  // On button press, send in this buzzer's number
+  if(digitalRead(buzzerBtn) == LOW) {
+    Serial.println(buzzerNumber);
+    delay(1000);  //pause for a while
+  }
+  else if (Serial.available()) {
+    int n = Serial.parseInt();
+    if (n != 0) {
+      buzzerNumber = n;
+      Serial.print("Button number is now ");
+    }
+    else {
+      Serial.print("Error: NaN. Button number is still ");
+    }
+    Serial.println(buzzerNumber);
+    delay(1000);
+  }
+}
